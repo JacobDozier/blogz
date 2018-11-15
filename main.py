6c931b0418d8d48bd4b38cb2da74f6fba84acf5b
Jacob Dozier <jacobadozier@gmail.com>
2018-11-14 22:56:49 -0600

    Created Blog class and app route /.

--- main.py
+++ main.py
@@ -4,7 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['DEBUG'] = True
 # Change the database user pass and db name.
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:get-it-done@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = ''
+app.secret_key = '%9VK@b2Bt^gu'
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(255))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+    
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    blog_posts = Blog.query.all()
+    return render_template('posts.html', page_title="Build a Blog", blog_posts=blog_posts)
+    
