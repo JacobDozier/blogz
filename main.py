31de9313304e6b1a267c85d90800d5572cf0b6fe
Jacob Dozier <jacobadozier@gmail.com>
2018-11-19 13:10:08 -0600

    Second attempt at styling the nav links.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-#app.secret_key = '%9VK@b2Bt^gu'
+app.secret_key = '%9VK@b2Bt^gu'
 
 class Blog(db.Model):
 
