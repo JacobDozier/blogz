c93b7711940c18630123bbdb12eda67c05c0b881
Jacob Dozier <jacobadozier@gmail.com>
2018-11-18 23:33:09 -0600

    Tooled around with the css to space the links out, unsuccessfully. Also, added the run command at the end of main.py.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = '%9VK@b2Bt^gu'
+#app.secret_key = '%9VK@b2Bt^gu'
 
 class Blog(db.Model):
 
@@ -20,11 +20,14 @@ class Blog(db.Model):
 
 @app.route('/newpost', methods=['POST'])
 def new_post():
-    
+    return redirect('/blog')
     
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     blog_posts = Blog.query.all()
     return render_template('posts.html', page_title="Build a Blog", blog_posts=blog_posts)
-    
+
+
+if __name__ == '__main__':
+    app.run()
