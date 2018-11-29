0b8b9b65bbee5fbe67d3e277f95b5df5ebf00d1d
Jacob Dozier <jacobadozier@gmail.com>
2018-11-28 21:34:58 -0600

    Reworked db classes, filled in the login page, and added the app.before_request.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -14,6 +15,7 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(255))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    time_stamp = db.Column(db.DateTime)
 
     def __init__(self, title, body, owner_id):
         self.title = title
@@ -23,28 +25,45 @@ class Blog(db.Model):
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(25))
+    username = db.Column(db.String(25), unique=True)
     password = db.Column(db.String(25))
-    blogs = db.Column(db.Integer, db.ForeignKey('blog.id'))
+    blogs = db.relationship('Blog', backref='owner')
 
     def __init__(self, email, password, blogs):
         self.email = email
         self.password = password
-        self.blogs = blogs
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return
+    return render_template('signup.html')
+
+@app.before_request
+def require_login():
+    allowed_routes = ['index', 'login', 'signup', 'main_blog']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return
+    # if request.method == 'POST':
+    #     email = request.form['email']
+    #     password = request.form['password']
+    #     user = User.query.filter_by(email=email).first()
+    # if user and user.password == password:
+    #     session['email'] = email
+    #     flash("Logged in")
+    #     return redirect('/')
+    # else:
+    #     flash('User password incorrect, or user does not exist', 'error')
+
+    return render_template('login.html')
 
 @app.route('/newpost', methods=['GET', 'POST'])
 def new_post():
     if request.method == 'POST':
         blog_title = request.form['title']
         blog_body = request.form['body']
+        blog_owner = User.query.filter_by(username=session['username']).first()
 
         # Checking for blanks and rerenders with error messages.
         if not blog_title:
@@ -57,12 +76,11 @@ def new_post():
                 body_error = "Some text is required."
                 return render_template('newpost.html', body_error=body_error, blog_title=blog_title)
 
-        # Need to add blog id when creating the new_post.
-        new_post = Blog(blog_title, blog_body)
+        new_post = Blog(blog_title, blog_body, blog_owner)
+
         db.session.add(new_post)
         db.session.commit()
-        id = new_post.id
-        return redirect('/blog?id={0}'.format(id))
+        return redirect('/blog?id={0}'.format(str(new_post.id)))
         
     return render_template('newpost.html')
 
@@ -81,7 +99,7 @@ def main_blog():
 def index():
 
     blog_posts = Blog.query.all()
-    return render_template('posts.html', page_title="Build a Blog", blog_posts=blog_posts)
+    return render_template('posts.html', blog_posts=blog_posts)
 
 
 if __name__ == '__main__':
